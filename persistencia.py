6e474bc782acfa79954e169eaa678f0d6be9ca82
VFox21 <94406579+VFox21@users.noreply.github.com>
2021-12-16 19:41:40 -0600

    Update persistencia.py

--- persistencia.py
+++ persistencia.py
@@ -3,6 +3,6 @@ def guardar_pedido(par_nombre,par_apellido):
     '''La funcion guardar_pedido implementa la persistencia en un fichero local.'''
 
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write("-" + par_nombre + " -"+ par_apellido)
+        file.write("-" + par_nombre + " " + par_apellido + "\n")
         file.close()
     return True
