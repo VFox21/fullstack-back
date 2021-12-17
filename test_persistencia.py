c52d93a868c9af63a62282d464bb251226aa995b
VFox21 <94406579+VFox21@users.noreply.github.com>
2021-12-16 18:42:56 -0600

    Modificación test_persistencia con el -

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,23 +1,16 @@
-from persistencia import guardar_pedido
-
-def fn_clear_limpiar_fichero():
-    
-    with open("pedidos.txt", "w", encoding="utf-8") as file:
-        file.write("")
+"""
+Pruebas Persistencia
+"""
+import persistencia
+def test_guardar_pedido():
+    """
+    Prueba general
+    """
+    with open("pedidos.txt", "w+", encoding="utf-8") as file:
+        persistencia.guardar_pedido("Pedro", "Gil de Diego")
+        persistencia.guardar_pedido("Michael", "Jordan")
+        firstline = file.readline()
+        secondline = file.readline()
         file.close()
-    return True
-
-
-def fn_salvar_persistencia(diccionario):
-    if fn_clear_limpiar_fichero():
-        print(f"Documento pedidos.txt limpiado!")
-    
-    for item in diccionario:
-        print(f"El nombre es: {item['nombre']} y el apellido es {item['apellidos']}")
-        guardar_pedido(str(item['nombre']),str(item['apellidos']))
-
-
-#pedidos = [{"nombre": "Pedro", "apellidos": "Gil de Diego"},{"nombre": "Michael", "apellidos": "Jordan"},{"nombre": "Fabian", "apellidos": "Herrera"}]
-
-#fn_salvar_persistencia(pedidos)
-
+    assert firstline == "-Pedro Gil de Diego\n"
+    assert secondline == "-Michael Jordan\n"
