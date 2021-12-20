1dacb48f1eb507a72be6ec597a7e1d26ad169f79
VFox21 <94406579+VFox21@users.noreply.github.com>
2021-12-20 08:44:34 -0600

    Backend para soportar feature disponibilidad de tamanhos de pizzas

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-'''App.py para backend'''
+'''Backend para pedidos pizza fullstack'''
 from flask import Flask
 from flask import request
 from flask import redirect
-#from test_persistencia import fn_salvar_persistencia
+from flask import Response
+from test_persistencia import fn_salvar_persistencia
 
 app = Flask(__name__)
 
@@ -10,8 +11,6 @@ app = Flask(__name__)
 @app.route("/pizza",methods=['POST'])
 
 def pizza():
-    '''Se implementa el listener para el contexto pizza.'''
-
     nombre = str(request.form.get("nombre_cliente"))
     apellido = str(request.form.get("apellido_cliente"))
 
@@ -20,7 +19,24 @@ def pizza():
     pedidos = [diccionario]
     print(f"Pedidos: {pedidos}")
 
-    #fn_salvar_persistencia(pedidos)
+    fn_salvar_persistencia(pedidos)
 
     #return "<html><body><p>Hello "+ nombre +"!</p></body></html>"
     return redirect("http://localhost/solicita_pedido.html", code=302)
+
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+
+    size = str(request.form.get("tamanho"))
+    respuesta_negativa = "No disponible"
+    respuesta_positiva = "Disponible"
+
+    if size == 'S':
+        #return redirect("No disponible")
+        return Response(respuesta_negativa, 200, {'Access-Control-Allow-Origin': '*'})
+    else:
+        return Response(respuesta_positiva, 200, {'Access-Control-Allow-Origin': '*'})
+        #return redirect("Disponible")
